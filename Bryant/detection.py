0e3bdc237b2d8d66365dad2119b5d8eb5ef2a84a
Bryant <brycornw@iu.edu>
2022-04-03 15:38:15 -0400

    Bounding box and label xml for sample_1.png

--- Bryant/detection.py
+++ Bryant/detection.py
@@ -6,6 +6,10 @@ def main():
 
     # Add model and detection points for bounding box
 
+    # One model for the people on field
+
+    # one model for ball
+
     # Bounding box code (topleft, bottomright)
     pt1, pt2 = (50, 50), (100, 100)
     cv2.rectangle(img, pt1, pt2, color=(0, 255, 0), thickness=2)
