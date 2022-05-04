e0f580da06b03283f532b875ddae61552dc8f3a5
samize <37714272+samize@users.noreply.github.com>
2022-05-03 21:51:41 -0400

    fixed my old code shit

--- Seth/code/projection_functional.py
+++ Seth/code/projection_functional.py
@@ -7,11 +7,10 @@ import json
 import sys
 
 import cv2
-import numpy as np
+from tqdm import tqdm
 
-import load_above
-from part2 import get_projection_matrix, test_transition_matrix, get_transition_matrix, convert_vector
-from part2 import main as part2main
+from load_above import *
+from part2 import main, get_projection_matrix, test_transition_matrix, get_transition_matrix, convert_vector
 
 
 def use_pairing(pairings):
@@ -27,42 +26,29 @@ def use_pairing(pairings):
 
     return True
 
-def create_overhead_projection(frame_file: Path, corner_pixels_file: Path, field_file: Path, output_file: Path):
-
-    if not isinstance(frame_file, Path):
-        frame_file = Path(frame_file)
-
-    if not isinstance(corner_pixels_file, Path):
-        corner_pixels_file = Path(corner_pixels_file)
-
-    if not isinstance(field_file, Path):
-        field_file = Path(field_file)
-
-    if not isinstance(output_file, Path):
-        output_file = Path(output_file)
-
-    if not output_file.parent.exists():
-        output_file.parent.mkdir(parents=True, exist_ok=True)
-
-    image = cv2.imread(str(frame_file))
-    ground, red_pixels, line_pixels = load_above.main(field_file)
-    
-    with open(corner_pixels_file, 'r') as file:
-        pixels = json.loads(file.read())
-    #ground = cv2.imread('above-black-and-red.png')
-
-    #pixels = [(592, 229), (331, 173), (1013, 637), (981, 393), (420, 256)]
-
-    #red_pixels = [(79, 236), (79, 269), (83, 99), (83, 104), (83, 162), (83, 212), (83, 236), (83, 269), (83, 294), (83, 343), (83, 401), (83, 406), (88, 99), (88, 406), (108, 212), (108, 294), (158, 162), (158, 220), (158, 221), (158, 284), (158, 285), (158, 343), (320, 99), (320, 211), (320, 212),
-    #              (320, 294), (320, 406), (482, 162), (482, 220), (482, 221), (482, 284), (482, 285), (482, 343), (532, 212), (532, 294), (552, 99), (552, 406), (557, 99), (557, 104), (557, 162), (557, 212), (557, 236), (557, 269), (557, 294), (557, 343), (557, 401), (557, 406), (561, 236), (561, 269)]
 
 
-    #red_pixels = [(482, 343), 
-    #              (532, 212), (532, 294), 
-    #              (557, 162), (557, 212)]
+if __name__ == '__main__':
 
-    #red_pixels = [(482, 343), (532, 212), (532, 294), (557, 162), (557, 212)]
+    warnings.filterwarnings("ignore")
 
+    input_image = sys.argv[1]
+    above_image = sys.argv[2]
+    if len(sys.argv) > 3:
+        pixel_map = sys.argv[3]
+    else:
+        pixel_map = None
+
+    image = cv2.imread(input_image)
+    ground = cv2.imread(above_image)
+
+    pixels = [(592, 229), (331, 173), (1013, 637), (981, 393), (420, 256)]
+
+    red_pixels = [(79, 236), (79, 269), (83, 99), (83, 104), (83, 162), (83, 212), (83, 236), (83, 269), (83, 294), (83, 343), (83, 401), (83, 406), (88, 99), (88, 406), (108, 212), (108, 294), (158, 162), (158, 220), (158, 221), (158, 284), (158, 285), (158, 343), (320, 99), (320, 211), (320, 212),
+                  (320, 294), (320, 406), (482, 162), (482, 220), (482, 221), (482, 284), (482, 285), (482, 343), (532, 212), (532, 294), (552, 99), (552, 406), (557, 99), (557, 104), (557, 162), (557, 212), (557, 236), (557, 269), (557, 294), (557, 343), (557, 401), (557, 406), (561, 236), (561, 269)]
+
+
+    red_pixels = [(482, 343), (532, 212), (532, 294), (557, 162), (557, 212)]
 
     pixel_pairs = list(itertools.product(pixels, red_pixels))
 
@@ -72,7 +58,13 @@ def create_overhead_projection(frame_file: Path, corner_pixels_file: Path, field
     best_matrix = None
     match_counts = {}
 
-    for index, combination in enumerate(itertools.combinations(pixel_pairs, 4)):
+    combinations = itertools.combinations(pixel_pairs, 4)
+
+    if pixel_map is not None:
+        with open(pixel_map, 'r') as file:
+            combinations = [json.loads(file.read())]
+
+    for index, combination in enumerate(combinations):
 
         #if index < 7100 or index > 7450:
         #   continue
@@ -127,7 +119,7 @@ def create_overhead_projection(frame_file: Path, corner_pixels_file: Path, field
                 if skip:
                     break
                 pixel = ground[y, x, :]
-                if load_above.is_dark_pixel(pixel):
+                if is_dark_pixel(pixel):
                     continue
                 new_point = np.matmul(transition_matrix, np.array([x, y, 1]))
                 x_, y_ = new_point[0] / new_point[2], new_point[1] / new_point[2]
@@ -137,7 +129,7 @@ def create_overhead_projection(frame_file: Path, corner_pixels_file: Path, field
                     skip = True
                     break
 
-                if 0 <= x_ and x_ < image_copy.shape[1] and 0 <= y_ and y_ < image_copy.shape[0] and load_above.is_white_pixel(image_copy[y_,x_,:], threshold=175):
+                if 0 <= x_ and x_ < image_copy.shape[1] and 0 <= y_ and y_ < image_copy.shape[0] and is_white_pixel(image_copy[y_,x_,:], threshold=175):
                     matches += 1
                     image_copy = cv2.circle(image_copy, (x_,y_), 3, (0,0,255), -1)
                     #image_copy[y_, x_, :] = (0,0,255)
@@ -153,7 +145,6 @@ def create_overhead_projection(frame_file: Path, corner_pixels_file: Path, field
 
         match_counts[index] = matches
     
-    output_directory = output_file.parent
     with open('matches.json', 'w+') as file:
         file.write(json.dumps(match_counts, indent=4))
 
@@ -164,20 +155,10 @@ def create_overhead_projection(frame_file: Path, corner_pixels_file: Path, field
 
     for x in range(broadcast.shape[1]):
         for y in range(broadcast.shape[0]):
-            if load_above.is_black_pixel(broadcast[y,x,:], threshold=0):
+            if is_black_pixel(broadcast[y,x,:], threshold=0):
                 new_point = np.matmul(best_matrix, np.array([x,y,1]))
                 new_x, new_y = new_point[0] / new_point[2], new_point[1] / new_point[2]
                 new_x, new_y = int(new_x), int(new_y)
                 ground = cv2.circle(ground, (new_x, new_y), 1, (0,0,255), -1)
 
-    cv2.imwrite(output_file, ground)
-
-if __name__ == '__main__':
-
-    warnings.filterwarnings("ignore")
-
-    frame_file = Path(sys.agrv[1])
-    corner_pixels_file = Path(sys.argv[2])
-    field_file = Path(sys.argv[3])
-    output_file = Path(sys.argv[4])
-
+    cv2.imwrite('mapped.jpg', ground)
